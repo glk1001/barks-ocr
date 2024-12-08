42a2047f6cf5f2169262e06d95cd5176c76321b6
glk1001 <gregg.kay@gmail.com>
2024-12-08 16:36:47 +1100

    Improve corrections, and print summary at end.

--- ocr_example.py
+++ ocr_example.py
@@ -1,24 +1,57 @@
 import sys
-from typing import Tuple
+from typing import Tuple, List
 
 import autocorrect
 import cv2 as cv
 import easyocr
 import enchant
 import numpy as np
-import pytesseract
 from PIL import Image
 from PIL import ImageDraw
 
-REJECTED_WORDS = ["OO", "|", "L", "\\", "IY"]
+REJECTED_WORDS = ["F", "H", "M", "W", "OO", "VV", "|", "L", "\\", "IY"]
+AUTO_CORRECTIONS = {
+    "AOINT MARROW": "POINT MARROW",
+    "FIZZLEBUDEET": "FIZZLEBUDGET",
+    "G0": "GO",
+}
 
 spell_dict = enchant.DictWithPWL("en_US", "mywords.txt")
 spell_correct = autocorrect.Speller()
 
 
+def words_are_ok(words_str: str) -> Tuple[bool, List[str]]:
+    words_str = words_str.strip(" ")
+
+    auto_ok, corrected_words_str = can_auto_correct(words_str)
+    if auto_ok:
+        return True, [corrected_words_str]
+
+    words = words_str.split(" ")
+
+    accepted_words = []
+    for word in words:
+        word_ok, accepted_word = word_is_ok(word)
+        if not word_ok:
+            return False, []
+        accepted_words.append(accepted_word)
+
+    return True, accepted_words
+
+
+def can_auto_correct(words_str: str) -> Tuple[bool, str]:
+    if words_str in AUTO_CORRECTIONS:
+        return True, AUTO_CORRECTIONS[words_str]
+
+    if words_str[-1] in ").!;?,":
+        if words_str[:-1] in AUTO_CORRECTIONS:
+            return True, AUTO_CORRECTIONS[words_str[:-1]] + words_str[-1]
+
+    return False, ""
+
+
 def word_is_ok(word: str) -> Tuple[bool, str]:
     word = word.upper().strip()
-    orig_word = word
 
     if not word:
         return False, ""
@@ -28,9 +61,10 @@ def word_is_ok(word: str) -> Tuple[bool, str]:
 
     if spell_dict.check(word):
         return True, word
-    word = word.rstrip(").!;?,")
-    if spell_dict.check(word):
-        return True, orig_word
+
+    if word[-1] in ").!;?,":
+        if spell_dict.check(word[:-1]):
+            return True, word
 
     possible_words = spell_dict.suggest(word)
     possible_words = [f'"{word}"' for word in possible_words]
@@ -45,91 +79,42 @@ def word_is_ok(word: str) -> Tuple[bool, str]:
     return True, word
 
 
-def get_easyocr_text_box_data(image_file: str):
+def get_easyocr_text_box_data(image_file: str) -> List[Tuple[List[int], str, str, float]]:
     reader = easyocr.Reader(["en"])
     result = reader.readtext(image_file)
 
-    txt_boxes = []
+    text_list = []
     for bbox, text, prob in result:
         (tl, tr, br, bl) = bbox
-        print(
-            f'prob = {prob:4.2f}, text = "{text}", Box:'
-            f" {bl[0]:.0f}, {bl[1]:.0f}, {br[0]:.0f}, {br[1]:.0f},"
-            f" {tl[0]:.0f}, {tl[1]:.0f}, {tr[0]:.0f}, {tr[1]:.0f}"
-        )
 
         text_str = text.strip()
         if prob < 0.1 or not text_str:
             continue
 
-        words = text_str.split(" ")
-        accepted_words = []
-        text_ok = True
-        for word in words:
-            text_ok, accepted_word = word_is_ok(word)
-            if not text_ok:
-                break
-            accepted_words.append(accepted_word)
-        if not text_ok:
+        words_ok, accepted_words = words_are_ok(text_str)
+        if not words_ok:
             continue
+        accepted_words_str = " ".join(accepted_words)
 
-        print(f'prob = {prob:4.2f}, text = "{" ".join(accepted_words)}" -- ACCEPTED')
-        x0 = tl[0]
-        y0 = tl[1]
-        x1 = tr[0]
-        y1 = tr[1]
-        x2 = br[0]
-        y2 = br[1]
-        x3 = bl[0]
-        y3 = bl[1]
-        txt_boxes.append([x0, y0, x1, y1, x2, y2, x3, y3])
+        x0 = int(round(tl[0]))
+        y0 = int(round(tl[1]))
+        x1 = int(round(tr[0]))
+        y1 = int(round(tr[1]))
+        x2 = int(round(br[0]))
+        y2 = int(round(br[1]))
+        x3 = int(round(bl[0]))
+        y3 = int(round(bl[1]))
+        bbox = [x0, y0, x1, y1, x2, y2, x3, y3]
 
-        # txt_boxes.append([x0, y0, x1, y1])
-        # if y0 > y1:
-        #     print(f"ERROR: y0, {y0} > {y1}.")
+        text_list.append((bbox, text_str, accepted_words_str, prob))
 
-    return txt_boxes
+    return text_list
 
 
-def get_text_box_data(img):
-    config = r"--oem 1 --psm 6"  # 3,4,6,11
-
-    d = pytesseract.image_to_data(img, output_type=pytesseract.Output.DICT, config=config)
-    print(d)
-
-    txt_boxes = []
-    n_boxes = len(d["level"])
-    for i in range(n_boxes):
-        text_str = d["text"][i].strip()
-        print(f'i: {i} - conf = {d["conf"][i]}, text = "{text_str}"')
-        if d["conf"][i] == -1:
-            continue
-        if d["conf"][i] < 30 or not text_str:
-            continue
-        if not word_is_ok(text_str):
-            continue
-        print(f'i: {i} - conf = {d["conf"][i]}, text = "{text_str}" -- ACCEPTED')
-        (x, y, w, h) = (d["left"][i], d["top"][i], d["width"][i], d["height"][i])
-        txt_boxes.append([x, y, x + w, y + h])
-
-    return txt_boxes
-
-
-def get_text_boxes(img):
-    h, w, _ = img.shape  # assumes color image
-
-    boxes = pytesseract.image_to_boxes(img)  # also include any config options you use
-
-    txt_boxes = []
-    for b in boxes.splitlines():
-        b = b.split(" ")
-        x1 = int(b[1])
-        y1 = h - int(b[2])
-        x2 = int(b[3])
-        y2 = h - int(b[4])
-        txt_boxes.append([x1, y1, x2, y2])
-
-    return txt_boxes
+def get_box_str(box: List[int]) -> str:
+    assert len(box) == 8
+    return f"{box[0]:04},{box[1]:04}, {box[2]:04},{box[3]:04}, "\
+           f"{box[4]:04},{box[5]:04}, {box[6]:04},{box[7]:04}"
 
 
 def get_bw_image(file: str) -> cv.typing.MatLike:
@@ -153,35 +138,19 @@ if __name__ == "__main__":
     grey_image_file = "/tmp/image_grey.png"
     cv.imwrite(grey_image_file, bw_image)
 
-    # tesseract_text = pytesseract.image_to_string(image)
-    # print(tesseract_text)
-
-    # text_boxes = get_text_boxes(image)
-    # print(text_boxes)
-
-    # pil_image = Image.fromarray(image)
-    # img_rects = ImageDraw.Draw(pil_image)
-    # for box in text_boxes:
-    #     shape = [(box[0], box[3]), (box[2], box[1])]
-    #     img_rects.rectangle(shape, outline="red")
-    #
-    # img_rects._image.save("/tmp/bubbles-text-boxes.png")
-
-    # text_data_boxes = get_text_box_data(image)
-    #
-    # pil_image = Image.fromarray(image)
-    # img_rects = ImageDraw.Draw(pil_image)
-    # for box in text_data_boxes:
-    #     shape = [(box[0], box[1]), (box[2], box[3])]
-    #     img_rects.rectangle(shape, outline="green", width=5)
-    #
-    # img_rects._image.save("/tmp/bubbles-text-data-boxes.png")
-
     text_data_boxes = get_easyocr_text_box_data(grey_image_file)
 
     pil_image = Image.fromarray(cv.merge([bw_image, bw_image, bw_image]))
     img_rects = ImageDraw.Draw(pil_image)
-    for box in text_data_boxes:
+    for box, _, _, _ in text_data_boxes:
         img_rects.polygon(box, outline="green", width=5)
-
     img_rects._image.save("/tmp/bubbles-text-data-boxes-easy.png")
+
+    max_test_len = max([len(t[1]) for t in text_data_boxes])
+    max_acc_test_len = max([len(t[2]) for t in text_data_boxes])
+    print()
+    for box, text, accepted_text, prob in text_data_boxes:
+        print(
+            f'text: "{text:<{max_test_len}}", acc: "{accepted_text:<{max_acc_test_len}}"'
+            f' P: {prob:4.2f}, box: "{get_box_str(box)}"'
+        )
