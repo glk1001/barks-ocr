c7523a1b5d3ea6667adcf58e274750cd4cb0f78d
glk1001 <gregg.kay@gmail.com>
2025-09-02 12:15:41 +1000

    Made nano more easily usable.

--- src/nano-banana-test.py
+++ src/nano-banana-test.py
@@ -1,55 +1,109 @@
 import os
-import sys
+from enum import Enum, auto
 from pathlib import Path
 
 from PIL import Image
 from io import BytesIO
 
 from google import genai
-from google.genai import types
 from google.genai.types import GenerateContentConfig
 
-SYSTEM_INSTRUCTION = "Do not remove any part of characters"
+
+class Prompts(Enum):
+    REMOVE_SPEECH_BUBBLES = auto()
+    REMOVE_NARRATION_AND_SPEECH_BUBBLES = auto()
+    COLORIZE_WITH_GRADIENTS = auto()
+    MAKE_PHOTO_REALISTIC = auto()
+    MAKE_OIL_PAINTING = auto()
+    EXTRACT_TEXT = auto()
+
+
+REMOVE_BUBBLE_DEST_SUFFIX = "no-bubbles.png"
+RECOLOR_DEST_SUFFIX = "-recolor.png"
+PHOTO_DEST_SUFFIX = "-photo.png"
+OIL_DEST_SUFFIX = "-oil.png"
+EXTRACT_TEXT_DEST_SUFFIX = "-just-text.txt"
+
+PROMPT_TEXT = {
+    Prompts.REMOVE_SPEECH_BUBBLES: (
+        "Remove all speech bubbles.",
+        REMOVE_BUBBLE_DEST_SUFFIX,
+    ),
+    Prompts.REMOVE_NARRATION_AND_SPEECH_BUBBLES: (
+        "Remove all speech bubbles and narrator box.",
+        REMOVE_BUBBLE_DEST_SUFFIX,
+    ),
+    Prompts.COLORIZE_WITH_GRADIENTS: (
+        "Colorize this using color gradients.",
+        RECOLOR_DEST_SUFFIX,
+    ),
+    Prompts.MAKE_PHOTO_REALISTIC: (
+        "Make this look like a super-realistic 3D photograph.",
+        PHOTO_DEST_SUFFIX,
+    ),
+    Prompts.MAKE_OIL_PAINTING: (
+        "Make this look like a very realistic and beautiful oil painting.",
+        OIL_DEST_SUFFIX,
+    ),
+    Prompts.EXTRACT_TEXT: (
+        "Extract the text from this image.",
+        EXTRACT_TEXT_DEST_SUFFIX,
+    ),
+}
+# PROMPT = "Remove the smaller inner panel."
+# PROMPT = "Improve this comic book cover."  # bit of a dud
+
+PROMPT_TO_USE = Prompts.MAKE_OIL_PAINTING
+
+SYSTEM_INSTRUCTION = "Do not remove any part of characters. Do not vignette."
 CANDIDATE_COUNT = 1
-AI_TEMPERATURE = 0.1
+AI_TEMPERATURE = 0.9
 AI_TOP_P = None
 AI_TOP_K = None
 SEED = None
 
 ROOT_DIR = Path("/home/greg/Books/Carl Barks")
 BARKS_PANELS_PNG = ROOT_DIR / "Barks Panels Pngs"
-#PANEL_TYPE = "Favourites"
+FANTA_RESTORED_DIR = ROOT_DIR / "Fantagraphics-restored"
+# PANEL_TYPE = "Favourites"
 PANEL_TYPE = "Splash"
-TITLE ="Only a Poor Old Man"
+TITLE = "Tralla La"
 EDITED = "edited"
-#EDITED = ""
-IMAGE_FILENAME = "252.png"
+# EDITED = ""
+IMAGE_FILENAME = "255-big-3.png"
 
-#SRCE_IMAGE="/home/greg/Books/Carl Barks/Fantagraphics-fixes-and-additions/Carl Barks Vol. 8 - Donald Duck - Trail of the Unicorn (Digital-Empire)/images/245.png"
+DEST_SUFFIX = PROMPT_TEXT[PROMPT_TO_USE][1]
+PROMPT_STR = PROMPT_TEXT[PROMPT_TO_USE][0]
+
+PROMPT_STR += " Make the trees in the foreground taller. Make the sky darker with more clouds."
+#PROMPT_STR += " Add a some extra trees and rocks in the foreground. Add some more clouds to the sky."
+#PROMPT_STR += " Outpaint the top and bottom of the image to give a consistent scene that seamlessly matches the middle. Crucially make color and structure match."
+#SYSTEM_INSTRUCTION += " Change the shape of the output image so that it's 1000 pixels wide and 1600 pixels high."
+
+# SRCE_IMAGE="/home/greg/Books/Carl Barks/Fantagraphics-fixes-and-additions/Carl Barks Vol. 8 - Donald Duck - Trail of the Unicorn (Digital-Empire)/images/245.png"
 SRCE_IMAGE = ROOT_DIR / BARKS_PANELS_PNG / PANEL_TYPE / TITLE / EDITED / IMAGE_FILENAME
-#SRCE_IMAGE = "/tmp/255.png"
-PROMPT = "Colorize this using color gradients."
-#PROMPT = "Make this look like a very realistic 3D photograph."
-#PROMPT = "Remove all speech bubbles."
-#PROMPT = "Remove all speech bubbles and narrator box."
-#PROMPT = "Remove the smaller inner panel."
-#PROMPT = "Extract the text from this image."
-#PROMPT = "Improve this comic book cover."  # bit of a dud
-DEST_SUFFIX="-recolor.png"
-#DEST_SUFFIX="-photo.png"
 
 # SRCE_IMAGE = "/home/greg/Books/Carl Barks/Fantagraphics-censorship-fixes/wdcs-34/01.png"
 # PROMPT = ("This is a scanned image. Cleanup and correct any warping caused by scanning. Keep the"
 #           " output resolution the same as the input resolution."
 #           " Don't crop any panel borders.")
 
-dest_image = ROOT_DIR / BARKS_PANELS_PNG / 'AI' / TITLE / EDITED / (SRCE_IMAGE.stem + DEST_SUFFIX)
+dest_image = (
+    ROOT_DIR
+    / BARKS_PANELS_PNG
+    / "AI"
+    / TITLE
+    / EDITED
+    / (SRCE_IMAGE.stem + DEST_SUFFIX)
+)
 dest_image.parent.mkdir(parents=True, exist_ok=True)
 if not dest_image.parent.is_dir():
     raise FileNotFoundError(dest_image)
-print(f'Saving edited image to "{dest_image}"...')
 if dest_image.is_file():
     raise FileExistsError(dest_image)
+
+print(f"Prompt:\n    {PROMPT_STR}\n")
+print(f'Saving edited image to "{dest_image}"...')
 
 AI_MODEL = "gemini-2.5-flash-image-preview"
 
@@ -60,7 +114,7 @@ image = Image.open(SRCE_IMAGE, mode="r")
 
 response = client.models.generate_content(
     model=AI_MODEL,
-    contents=[PROMPT, image],
+    contents=[PROMPT_STR, image],
     config=GenerateContentConfig(
         system_instruction=SYSTEM_INSTRUCTION,
         candidate_count=CANDIDATE_COUNT,
